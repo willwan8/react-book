ef3c1454c070b502d262687bee0b9b45a5b0a429
willwan8 <williamwang1562@gmail.com>
2024-08-19 22:28:33 -0500

    completed chapter 8 of react book: interacting with a server

--- code-cafe/src/App.js
+++ code-cafe/src/App.js
@@ -1,8 +1,17 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import Header from './components/Header';
 import Home from './components/Home';
-import { items } from './items';
 
 function App() {
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    axios.get('/api/items')
+      .then((result) => setItems(result.data))
+      .catch(console.error);
+  }, []);
   return (
     <div>
       <Header />
